1d5a6495aeded002493cea506ffde2c183051d6b
Anzor-Ishak <anzori@spacelift.io>
2025-08-01 10:25:26 -0400

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,9 +1,17 @@
-resource "spacelift_stack" "meta_stack" {
-  name                  = "Version-Bump Demo"
-  terraform_workflow_tool = "OPEN_TOFU"
-  terraform_version       = "1.10.3"   # ← desired new binary
-  administrative          = true
+resource "spacelift_stack" "version-bump-demo" {
+  name = "Version-Bump Demo"
+  space_id = "root"
 
-  repository              = "adminstack"
-  branch                  = "main"
+  repository = "adminstack"
+  branch = "main"
+
+  terraform_version = "1.6.2"
+  terraform_workflow_tool = "OPEN_TOFU"
+  terraform_smart_sanitization = true
+
+  administrative = true
+  enable_well_known_secret_masking = true
+  github_action_deploy = false
 }
+
+
