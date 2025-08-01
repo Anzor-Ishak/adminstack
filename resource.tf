65d8f299bab1b00c99e200003427bd8ec518ab54
Anzor-Ishak <anzori@spacelift.io>
2025-08-01 10:14:28 -0400

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,6 @@
+resource "spacelift_stack" "meta_stack" {
+  name                  = "Version-Bump Demo"
+  terraform_workflow_tool = "OPEN_TOFU"
+  terraform_version       = "1.10.3"   # ← desired new binary
+  administrative          = true
+}
