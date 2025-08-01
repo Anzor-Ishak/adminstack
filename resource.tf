6aeb8e95cca3eacc2796d3119e0be137af3b50c6
Anzor-Ishak <anzori@spacelift.io>
2025-08-01 10:19:53 -0400

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -3,4 +3,7 @@ resource "spacelift_stack" "meta_stack" {
   terraform_workflow_tool = "OPEN_TOFU"
   terraform_version       = "1.10.3"   # ← desired new binary
   administrative          = true
+
+  repository              = "anzor-ishak/adminstack"
+  branch                  = "main"
 }
